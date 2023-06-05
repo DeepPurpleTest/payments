40229215b9c055abc7945b03f70b492324fe8a3a
Moksem <maks.viskovatov@gmail.com>
2023-06-05 17:13:40 +0300

    Fix builder, maven.yml and tests

--- src/test/java/com/example/payments/PaymentsApplicationTests.java
+++ src/test/java/com/example/payments/PaymentsApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.payments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class PaymentsApplicationTests {
+    @LocalServerPort
+    private Integer port;
 
     @Test
     void contextLoads() {
+        assertNotNull(port);
     }
 
 }
