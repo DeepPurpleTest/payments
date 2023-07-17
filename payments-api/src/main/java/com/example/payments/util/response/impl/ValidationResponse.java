0e5ece2602749e38db520a3d97c3b541dc6ce376
Moksem <maks.viskovatov@gmail.com>
2023-07-17 20:48:48 +0300

    Some ValidationResponse changes

--- payments-api/src/main/java/com/example/payments/util/response/impl/ValidationResponse.java
+++ payments-api/src/main/java/com/example/payments/util/response/impl/ValidationResponse.java
@@ -1,32 +1,28 @@
 package com.example.payments.util.response.impl;
 
 import com.example.payments.util.exception.EntityValidationException;
+import com.example.payments.util.response.ObjectError;
 import com.example.payments.util.response.ResponseWrapper;
 import lombok.Data;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Component
 @Data
 public class ValidationResponse implements ResponseWrapper<ValidationResponse, EntityValidationException> {
     private String message;
-    private Map<String, String> errors;
     private String timestamp;
+    private List<ObjectError> errors;
 
     @Override
     public ValidationResponse of(EntityValidationException ex) {
         this.message = ex.getMessage();
         this.errors = ex.getBindingResult().getAllErrors().stream()
-                .map(er -> (FieldError) er)
-                .collect(Collectors
-                        .toMap(
-                                FieldError::getField,
-                                FieldError::getDefaultMessage)
-                );
+                .map(er -> (org.springframework.validation.FieldError) er)
+                .map(er -> new ObjectError(er.getField(), er.getDefaultMessage()))
+                .toList();
         this.timestamp = LocalDateTime.now().toString();
         return this;
     }
