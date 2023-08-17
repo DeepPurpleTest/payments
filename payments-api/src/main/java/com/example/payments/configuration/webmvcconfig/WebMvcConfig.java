0a05ba1d94735976fbf8fb30c6e0735aac49fad2
Moksem <maks.viskovatov@gmail.com>
2023-08-17 20:35:02 +0300

    delete @EnableWebMvc, fix OutPaymentDto

--- payments-api/src/main/java/com/example/payments/configuration/webmvcconfig/WebMvcConfig.java
+++ payments-api/src/main/java/com/example/payments/configuration/webmvcconfig/WebMvcConfig.java
@@ -2,12 +2,10 @@ package com.example.payments.configuration.webmvcconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     // for find css folder
     @Override
--- payments-api/src/main/java/com/example/payments/dto/OutPaymentDto.java
+++ payments-api/src/main/java/com/example/payments/dto/OutPaymentDto.java
@@ -2,6 +2,7 @@ package com.example.payments.dto;
 
 import com.example.payments.dto.identifiable.AbstractOutPaymentIdentifiable;
 import com.example.payments.entity.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,5 +32,6 @@ public class OutPaymentDto implements AbstractOutPaymentIdentifiable {
     @NotNull
     private PaymentStatus status;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.ARRAY)
     private LocalDateTime date;
 }
