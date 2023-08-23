81fdd744bde55a893b65bd1f55c6eb6509e69ce8
Moksem <maks.viskovatov@gmail.com>
2023-08-23 20:03:01 +0300

    fix mongodb config

--- monitoring-service/src/main/java/com/example/monitoringservice/MonitoringServiceApplication.java
+++ monitoring-service/src/main/java/com/example/monitoringservice/MonitoringServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.monitoringservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-monitoring.properties")
 public class MonitoringServiceApplication {
 
     public static void main(String[] args) {
--- monitoring-service/src/main/java/com/example/monitoringservice/service/TimeCheckerService.java
+++ monitoring-service/src/main/java/com/example/monitoringservice/service/TimeCheckerService.java
@@ -3,14 +3,17 @@ package com.example.monitoringservice.service;
 import com.example.monitoringservice.entity.MethodExecutionTimeEvent;
 import com.example.monitoringservice.repository.TimeCheckerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TimeCheckerService {
     private final TimeCheckerRepository timeCheckerRepository;
+
     public List<MethodExecutionTimeEvent> getAllEvents() {
         return timeCheckerRepository.findAllBy();
     }
@@ -20,6 +23,10 @@ public class TimeCheckerService {
     }
 
     public void saveEvent(MethodExecutionTimeEvent event) {
+        log.info("TimeCheckerService.saveEvent() -> start");
         timeCheckerRepository.save(event);
+        System.out.println(timeCheckerRepository.findAllBy().size());
+        log.info("TimeCheckerService.saveEvent() -> end");
+
     }
 }
